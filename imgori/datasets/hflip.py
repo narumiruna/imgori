a18de460c69efaedc0a49d5188098f1b6fbc94d7
narumi <4680567+narumiruna@users.noreply.github.com>
2023-12-12 16:07:53 +0800

    cache images

--- imgori/datasets/hflip.py
+++ imgori/datasets/hflip.py
@@ -3,13 +3,16 @@ from pathlib import Path
 from typing import Callable
 from typing import List
 
+from loguru import logger
 from mlconfig import register
 from PIL import Image
 from PIL import ImageOps
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from torchvision import transforms
+from tqdm import tqdm
 
+from ..typing import PathLike
 from ..typing import PILImage
 
 
@@ -18,24 +21,35 @@ def get_image_extensions() -> List[str]:
     return list(Image.EXTENSION.keys())
 
 
+def read_image(f: PathLike) -> PILImage:
+    return Image.open(f).convert("RGB")
+
+
 class RandomHorizontalFlipDataset(Dataset):
-    def __init__(self, root: str, transform: Callable, p: float = 0.5) -> None:
+    def __init__(
+        self, root: str, transform: Callable, p: float = 0.5, cache: bool = True
+    ) -> None:
         self.root = Path(root)
         self.transform = transform
         self.p = p
+        self.cache = cache
 
         if self.p < 0 or self.p > 1:
             raise ValueError("p must be in [0, 1]")
 
         exts = get_image_extensions()
-        self.paths = [p for p in self.root.rglob("*") if p.suffix in exts]
+        self.images = [p for p in self.root.rglob("*") if p.suffix in exts]
+        if cache:
+            logger.info("cache images")
+            self.images = [read_image(p) for p in tqdm(self.images)]
 
     def __len__(self) -> int:
-        return len(self.paths)
+        return len(self.images)
 
     def __getitem__(self, index: int) -> (PILImage, int):
-        path = self.paths[index]
-        img = Image.open(path).convert("RGB")
+        img = self.images[index]
+        if not self.cache:
+            img = read_image(img)
 
         flip = random.random() < self.p
         if flip:
